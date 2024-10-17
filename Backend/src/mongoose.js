56b6102a8b83eeda31ffe037167a3e16c7567cb0
Sibichandru-R <sibichandru.rajendran@e5.ai>
2024-10-17 10:38:22 +0530

    Delete Backend directory

--- Backend/src/mongoose.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export const mongooseClient = async (app) => {
-  const connection = app.get('mongodb');
-  const database = new URL(connection).pathname.substring(1);
-  const mongooseConnection = await mongoose.connect(connection);
-};
--- Backend/src/services/index.js
+++
@@ -1,6 +0,0 @@
-import { user } from './users/users.js'
-export const services = (app) => {
-  app.configure(user)
-
-  // All services will be registered here
-}
